7bec533fb15054c862d85d26746df94bb1a405a6
Diegonascimento21 <diegonasc21@gmail.com>
2024-05-15 22:50:34 -0300

    Adicionando Tags/comentarios

---
+++ src/edu/diego/sintaxeJava/ComentariosTags.java
@@ -0,0 +1,29 @@
+package edu.diego.sintaxeJava;
+
+public class ComentariosTags {
+
+	public static void main(String[] args) {
+		/** Exemplo de comentário em bloco.
+		Podemos colocar tags como autor e datas para verificação de versão.
+		* @autor Diego Vieira
+		* @version 1.0
+		* @since 15/05/2024	
+		**/
+		
+		
+	}
+	
+	class Calculadora{
+		/** é possivel colocar comentários referenciado métodos.
+		 * @param num1
+		 * @param num2
+		 * @return int
+		 */
+		
+		public int somar(int num1, int num2) {
+			return num1 + num2;
+		}
+		
+	}
+	
+}
